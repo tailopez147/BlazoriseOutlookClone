d6039bb415ed82d4fa8abe42ad29f102e05a66b9
tailopez147 <49619133+tailopez147@users.noreply.github.com>
2026-03-12 00:48:02 +0300

    Create Program.cs

---
+++ BlazoriseOutlookClone.Client/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazorise;
+using Blazorise.FluentUI2;
+using Blazorise.Icons.FluentUI;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<BlazoriseOutlookClone.UI.App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddFluentUI2Components()
+    .AddFluentUIIcons();
+
+await builder.Build().RunAsync();
